6ea137848916fc2aaebb6c607b47783fa6ae92b4
Calvados <flamaster0807@gmail.com>
2018-09-26 19:42:11 +0300

    improved creating functions

--- raspberry-pi-apps/server/inet.cpp
+++ raspberry-pi-apps/server/inet.cpp
@@ -5,7 +5,10 @@ struct socket_inf* create_mcpsocket(const char* addr, short port)
 {
     struct socket_inf* sinf = new struct socket_inf;
 
-    sinf->fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (-1 == (sinf->fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP))) {
+        delete sinf;
+        return NULL;
+    }
     sinf->addinfo.sin_family = AF_INET;
     sinf->addinfo.sin_port = htons(port);
     sinf->addinfo.sin_addr.s_addr = inet_addr(addr);
@@ -16,9 +19,12 @@ struct socket_inf* create_mcpsocket(const char* addr, short port)
 
 struct epoll_inf*  create_epollinf(struct socket_inf* socket, uint32_t event_mode)
 {
-    struct epoll_inf einf = new struct epoll_inf;
+    struct epoll_inf* einf = new struct epoll_inf;
 
-    einf->id = epoll_create1(0);
+    if (-1 == (einf->id = epoll_create1(0))) {
+        delete einf;
+        return NULL;
+    }
     einf->epoll_event.data.fd = socket->fd;
     einf->epoll_event.events = event_mode;
 
